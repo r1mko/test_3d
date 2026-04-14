f3bf45dd623979b7650663b7ee9bacb09db6dd63
r1mko <r1mko@yahoo.com>
2026-04-14 22:45:09 +0400

    иницализация хексагонов

--- Assets/Scripts/Hexagon/Hexagon.cs
+++ Assets/Scripts/Hexagon/Hexagon.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Hexagon : MonoBehaviour
@@ -14,5 +13,47 @@ public class Hexagon : MonoBehaviour
         Yellow
     }
 
+    [Serializable]
+    public struct ColorMaterialPair
+    {
+        public HexagonColor color;
+        public Material material;
+    }
 
+    [SerializeField] private ColorMaterialPair[] colorMaterials;
+    [SerializeField] private Renderer hexRenderer;
+
+    private HexagonColor currentColor;
+
+    private void Awake()
+    {
+        if (hexRenderer == null)
+            hexRenderer = GetComponentInChildren<Renderer>();
+
+    }
+
+    public void Init(HexagonColor color)
+    {
+        currentColor = color;
+
+        // Находим соответствующий материал
+        foreach (var pair in colorMaterials)
+        {
+            if (pair.color == color)
+            {
+                if (hexRenderer != null)
+                {
+                    hexRenderer.material = pair.material;
+                }
+                return;
+            }
+        }
+
+        Debug.LogWarning($"Material for color {color} not found in Hexagon script.");
+    }
+
+    public HexagonColor GetColor()
+    {
+        return currentColor;
+    }
 }
--- Assets/Scripts/Stacks/FillStack.cs
+++ Assets/Scripts/Stacks/FillStack.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Hexagon;
 
 public class FillStack : MonoBehaviour
 {
+    // Шаг по высоте для каждого следующего блока
     private const float HeightStep = 0.075f;
 
     [SerializeField] private GameObject hexagonPrefab;
@@ -15,14 +19,62 @@ public class FillStack : MonoBehaviour
 
         int count = Random.Range(4, 8);
 
+        HexagonColor color1 = GetRandomColor();
+        HexagonColor? color2 = null;
+
+        if (Random.value < 0.3f)
+        {
+            color2 = GetRandomColorExcept(color1);
+        }
+
+        int splitIndex = count;
+        if (color2.HasValue)
+        {
+            splitIndex = count / 2;
+        }
+
         for (int i = 0; i < count; i++)
         {
             GameObject newBlock = Instantiate(hexagonPrefab, transform);
+
             Vector3 localPos = new Vector3(0f, i * HeightStep, 0f);
             newBlock.transform.localPosition = localPos;
-
             newBlock.transform.localRotation = Quaternion.identity;
             newBlock.transform.localScale = Vector3.one;
+
+            Hexagon hexComponent = newBlock.GetComponent<Hexagon>();
+            if (hexComponent != null)
+            {
+                HexagonColor chosenColor = (i < splitIndex) ? color1 : color2.Value;
+                hexComponent.Init(chosenColor);
+            }
         }
+    }
+
+    private HexagonColor GetRandomColor()
+    {
+        var values = System.Enum.GetValues(typeof(HexagonColor));
+        return (HexagonColor)values.GetValue(Random.Range(0, values.Length));
+    }
+
+    private HexagonColor GetRandomColorExcept(HexagonColor exclude)
+    {
+        var values = System.Enum.GetValues(typeof(HexagonColor));
+        List<HexagonColor> availableColors = new List<HexagonColor>();
+
+        foreach (HexagonColor c in values)
+        {
+            if (c != exclude)
+            {
+                availableColors.Add(c);
+            }
+        }
+
+        if (availableColors.Count > 0)
+        {
+            return availableColors[Random.Range(0, availableColors.Count)];
+        }
+
+        return exclude;
     }
 }
