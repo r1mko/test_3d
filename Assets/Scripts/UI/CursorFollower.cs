07e800f14240aecf1e422befc6088dcd89a9be69
r1mko <r1mko@yahoo.com>
2026-04-21 17:04:41 +0400

    курсор следования

---
+++ Assets/Scripts/UI/CursorFollower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CursorFollower : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+
+        if (_rectTransform == null)
+        {
+            Debug.LogError($"[CursorFollower] RectTransform not found on {gameObject.name}");
+        }
+    }
+
+    private void Update()
+    {
+        if (_rectTransform == null) return;
+
+        _rectTransform.position = Input.mousePosition;
+    }
+}
